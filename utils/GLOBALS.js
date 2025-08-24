e891021b58fabe34f9fdd033b7d4be37721f41ca
Shemdoe <georgehmariki@gmail.com>
2025-08-25 02:20:34 +0300

    updated

--- utils/GLOBALS.js
+++ utils/GLOBALS.js
@@ -2,7 +2,7 @@ const GLOBAL_VARS = {
   shemdoe: 741815228,
   imma: 1696890745,
   ADMINS: [741815228, 1696890745],
-  logs: -1002942692002
+  logs: -1002909981597
 };
 
 module.exports = GLOBAL_VARS;
